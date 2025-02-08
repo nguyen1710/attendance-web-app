f97ff4bd6526c0f725b94518f832e05fa1ad3df6
Ng T.Nguyen <nguyenthanhnguyen17102003@gmail.com>
2025-02-09 00:32:41 +0700

    add vercel file

--- backend/index.js
+++ backend/index.js
@@ -38,15 +38,15 @@ const corsOptions = {
   };
   
   // Lấy __dirname trong ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// console.log(__dirname)
-// Cấu hình express để phục vụ các tệp tĩnh trong thư mục public
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// // console.log(__dirname)
+// // Cấu hình express để phục vụ các tệp tĩnh trong thư mục public
+// app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-  });
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+//   });
 
   // Sử dụng middleware CORS
 app.use(cors(corsOptions));
