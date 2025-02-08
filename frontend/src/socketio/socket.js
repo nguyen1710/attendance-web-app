7d7e46320855740c8246eafde076bbc86dccf6c5
nguyen1710 <96380468+nguyen1710@users.noreply.github.com>
2025-02-08 23:14:05 +0700

    Update socket.js

--- frontend/src/socketio/socket.js
+++ frontend/src/socketio/socket.js
@@ -5,7 +5,11 @@ const API_URL_BASE = import.meta.env.VITE_API_BASE_URL;
 
 
 const socket = io.connect(API_URL_BASE)
-let cleanEmail = value.replace(/^"|"$/g, '');
-socket.emit("register", {email: cleanEmail});
+
+if(value) {
+  let cleanEmail = value.replace(/^"|"$/g, '');
+  socket.emit("register", {email: cleanEmail});
+}
+
 
 export default socket
