6a99041b5612a86c888add2962eb79c0ae5fb689
Ng T.Nguyen <nguyenthanhnguyen17102003@gmail.com>
2025-02-09 12:10:53 +0700

    fix cors admin

--- backend/routes/admin-service/admin.route.js
+++ backend/routes/admin-service/admin.route.js
@@ -1,10 +1,17 @@
 import { login, getAllClients, getAllClassrooms, updateProfile, getNewClients, deleteClient, blockClient, getClientsByStatus, 
     getClientsByEmail, getAttendanceByClassroomId, deleteAttendance, updateAttendance} from "../../controllers/admin-service/admin.controller.js";
-
+import cors from 'cors'
 import express from "express"
 const router = express.Router()
 
-router.post("/login", login)
+const corsOptionsPublic = {
+    origin: FRONTEND_URL,  // Chỉ cho phép frontend từ localhost:5173 truy cập
+    methods: ['GET', 'POST'],         // Các phương thức HTTP được phép cho các route công khai
+    allowedHeaders: ['Content-Type'], // Các header được phép
+    credentials: false,               // Không cần gửi cookie hay token cho các route này
+  };
+
+router.post("/login", cors(corsOptionsPublic),login)
 router.get("/getAllClients", getAllClients);
 router.get("/getNewClients", getNewClients)
 router.get("/getClientsByStatus", getClientsByStatus);
